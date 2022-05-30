5cc5cb80056d07ca6dd801ef0724b3b581d544f9
Matheus Vinute <matheus_vinute@hotmail.com>
2022-05-30 10:18:36 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+import './styles/global.scss';
+
+type Repository = {
+  id: number;
+  name: string;
+  favorite: boolean;
+}
+
+export function App() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function getRepositories(){
+    const response = await fetch('https://api.github.com/users/matheusvinute/repos');
+    const responseJson = await response.json();
+
+    setRepositories(responseJson);
+    console.log(responseJson);
+  }
+
+  useEffect(() => {
+    getRepositories();
+  }, [])
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `São ${filtered.length} Repositorios Favoritos`
+  }, [repositories])
+
+  function handleFavorite(id: number){
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+    })
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Github Explorer</h1>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favoritado)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>
+              Favorito
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
